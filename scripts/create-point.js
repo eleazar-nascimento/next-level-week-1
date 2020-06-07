ffd3b1959b0758234868f23832b902d0739062f9
zarathos-sky <zarathos.sky@gmail.com>
2020-06-07 11:38:39 -0300

    A Escolha da Stack - Desafio Feito

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -39,7 +39,7 @@ function getCities(event) {
         
         
         for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -51,3 +51,54 @@ document
     .querySelector('select[name=uf]')
     .addEventListener("change", getCities)
 
+
+// Items de coleta
+// pegar todos os li's
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    // adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
+
+    
+    // verificar se existem itens selecionados, se sim
+    // pegar os itens selecionados
+    
+    const alreadySelected = selectedItems.findIndex( (item ) => {
+        const itemFound = item == itemId // isso será true ou false
+        return itemFound
+    })
+
+    
+    // se já estiver selecionado, tirar da seleção
+    if(alreadySelected >= 0){
+        // tirar da seleção
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+        // se não estiver selecionado
+        // adicionar à seleção
+        selectedItems.push(itemId)
+    }
+
+
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+}
